d470fc31d500e899b3f298bbb1b869e5d0726073
Konain <konainraza105@gmail.com>
2024-03-24 16:15:02 +0500

    Assignment-5 done

--- Assignment-5/script.js
+++ Assignment-5/script.js
@@ -48,3 +48,74 @@ let FahreneightTOCelsius = (celsius * 5/9) * 32;
 document.write(` ${celsius}°C is ${celsiusTOFahreneight}°F<br>`);
 document.write(` ${fahrenheit}°F is ${FahreneightTOCelsius}°C <br>`);
 
+//Answer-7
+
+let item1 = {
+    price: 650,
+    quantity: 3,
+
+}
+
+let item2 = {
+    price: 100,
+    quantity: 7,
+
+}
+let shippingCharges = 100;
+document.write(`<h1>Shopping Cart</h1>. <br>`)
+
+document.write(`Price of Item 1 is ${item1.price}.<br> `)
+document.write(`Quantity of Item 1 is ${item1.quantity}.<br> `)
+document.write(`Price of Item 2 is ${item2.price}.<br> `)
+document.write(`Quantity of Item 2 is ${item2.quantity}.<br> `)
+document.write(`Shipping Address ${shippingCharges }.<br> `)
+document.write(`Total Cost of an order is ${item1.price*item1.quantity+ item2.price*item2.quantity+shippingCharges}<br> `)
+//Answer-8
+let totalMarks =980;
+let obtainedMarks= 804;
+document.write(`<h1>Marksheet</h1>. <br>`)
+
+document.write(`Total Marks: ${totalMarks}. <br>`)
+document.write(`Obtained Marks: ${obtainedMarks}. <br>`)
+document.write(`Percentage: ${obtainedMarks/totalMarks*100}%. <br>`)
+
+//Answer-9
+let usDollars = 10;
+let saudiRiyaals = 25;
+document.write(`<h1>Currency in PKR</h1>. <br>`)
+document.write(`Total Currency in PKR: ${usDollars*saudiRiyaals}. <br>`)
+
+//Answer-10
+
+let num = 10;
+let result =((num+5*num*10))/2;
+
+//Answer-11
+
+let currentYear = 2024;
+let birthYear = 2004;
+let age = currentYear - birthYear;
+
+document.write(`<h1>Age Calculator</h1><br>`)
+document.write(`Current Year: ${currentYear}<br>`);
+document.write(`Birth Year: ${birthYear}<br>`);
+document.write(`Your Age is: ${age} or ${age-1}<br>`);
+
+//Answer-12
+
+let radius = 20;
+document.write(`<h1>The Geometrizer</h1>`)
+
+document.write(`The Circumference is ${2 * 3.14 * radius}<br>`);
+document.write(`The Area is ${3.14 * radius*radius}<br>`);
+
+//Answer-13
+let favouriteSnack = "Lays";
+let estimatedAge = 90;
+let amountPerDay = 3;
+document.write(`<h1>The Lifetime Supply Calculator</h1><br>`)
+document.write(`Favourite Snack: ${favouriteSnack} <br>`)
+document.write(`Current Age: ${age} <br>`)
+document.write(`Estimated Age: ${estimatedAge}<br>`)
+document.write(`Amount of snacks per day: ${amountPerDay} <br>`)
+document.write(`YOu will need ${(estimatedAge-age)*amountPerDay} ${favouriteSnack} to last you unitl ripe old age of ${estimatedAge}<br>`)
