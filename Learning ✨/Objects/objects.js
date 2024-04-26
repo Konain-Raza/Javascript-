f5c18e589d98e1075af9112d12fbc97a5dc114ef
Konain <konainraza105@gmail.com>
2024-04-26 20:26:54 +0500

    completed learning objects

---
+++ Learning ✨/Objects/objects.js
@@ -0,0 +1,10 @@
+// Objects are key-value pairs where every key has a corresponding value; it can be an array, object, or any other data type.
+const person = { // Create a constant named 'person' and initialize it with an object.
+    firstName: "Konain",
+    lastName: "Raza",
+    age: 19,
+    eyeColor: "grey"
+  };
+  
+  person.age = 10; // We can also change its value by accessing it through the object name followed by a dot and its key.
+  console.log(person.age); // Print the value associated with the key 'age' in the object.
