3c6a85e6fe5c2839c8ffe6745219a801f501370b
Konain <konainraza105@gmail.com>
2024-06-12 23:37:04 +0500

    added some more readmes

--- Learning ✨/Spread Operator/spread-operator.js
+++
@@ -1,4 +0,0 @@
-// Spread operator (...) is used when we want to include all values.
-let fruitsBasket = ["banana", "apple", "grapes"];
-let allFruits = [...fruitsBasket, "pear", "pineapple"]; // Here, we use the spread operator to include all elements from the fruitBasket array in the allFruits array.
-console.log(allFruits)
--- Learning ✨/String/string-literals.js
+++
@@ -1,5 +0,0 @@
-let firstName = "Konain"
-let lastName = "Raza";
-
-console.log( firstName + " " + lastName); //if we want full name so we use string concatenation but there is one problem that we have to manually amange spaces for typography and we have add + operator this is oldest way so now we use string literals
-console.log(`my full name is ${firstName} ${lastName} `)// in this syntax we have used backticks ` and for variable value we use ${varName} we can also perform operation in it 
